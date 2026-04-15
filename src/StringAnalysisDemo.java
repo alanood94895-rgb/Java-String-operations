482e7604fede0749f647db1677b480047df35d59
CompanyProfile <alanood94895@gmail.com>
2026-04-15 14:12:09 +0400

    task 7:StringAnalysisDemo

---
+++ src/StringAnalysisDemo.java
@@ -0,0 +1,30 @@
+public class StringAnalysisDemo {
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Create a String variable with a sample text
+        String text1 = "Java Programming language";
+
+        // Display original string
+        System.out.println("Original String: " + text1);
+
+        // Use length() to display total number of characters
+        System.out.println("Length: " + text1.length());
+
+        // Create another String variable
+        String text2 = "Java Programming language";
+
+        // Use equals() to compare both strings
+        System.out.println("Are both strings equal" + text1.equals(text2));
+
+        // Use indexOf() to find position of a word
+        System.out.println("Position of 'Programming': " + text1.indexOf("Programming"));
+
+        // Use startsWith() to check beginning
+        System.out.println("Starts with 'Java'? " + text1.startsWith("Java"));
+
+        // Use endsWith() to check ending
+        System.out.println("Ends with 'language'? " + text1.endsWith("language"));
+    }
+}
