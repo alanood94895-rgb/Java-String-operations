ddc97c73d7325b6738f541fef525eab30882609f
CompanyProfile <alanood94895@gmail.com>
2026-04-15 15:00:12 +0400

    task 13:StringDoWhileDemo

---
+++ src/StringDoWhileDemo.java
@@ -0,0 +1,45 @@
+public class StringDoWhileDemo {
+    public static void main(String[] args) {
+
+        // Create a String variable
+        String text = "Java Programming 2026";
+
+        // Display original string
+        System.out.println("Original String: " + text);
+
+        // Use length()
+        System.out.println("Length: " + text.length());
+
+        // Counter
+        int i = 0;
+
+        // Counters
+        int lowercase = 0;
+        int spaces = 0;
+
+        // do-while loop
+        do {
+
+            // display each character directly
+            System.out.println(text.charAt(i));
+
+            // count lowercase letters directly using charAt()
+            if (text.charAt(i) >= 'a' && text.charAt(i) <= 'z') {
+                lowercase++;
+            }
+
+            // count spaces directly using charAt()
+            else if (text.charAt(i) == ' ') {
+                spaces++;
+            }
+
+            // increase counter
+            i++;
+
+        } while (i < text.length());
+
+        // display results
+        System.out.println("\nTotal Lowercase Letters: " + lowercase);
+        System.out.println("Total Spaces: " + spaces);
+    }
+}
