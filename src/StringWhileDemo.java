43239a42c1f5e97cbe9a416ed39ad46fef793e9a
CompanyProfile <alanood94895@gmail.com>
2026-04-15 14:52:36 +0400

    task 12:StringWhileDemo

---
+++ src/StringWhileDemo.java
@@ -0,0 +1,36 @@
+public class StringWhileDemo {
+    public static void main(String[] args) {
+
+        // Create a String variable
+        String text = "Java Programming 2026";
+
+        // Display original string
+        System.out.println("Original String: " + text);
+
+        // Use length() to find total characters
+        System.out.println("Length: " + text.length());
+
+        // Counter variable
+        int i = 0;
+
+        // Counters for spaces and uppercase letters
+        int spaces = 0;
+        int upperCase = 0;
+
+        // while loop
+        while (i < text.length()) {
+
+            // use charAt()
+
+            System.out.println("ava Programming 2026");
+            String word = "Java";
+            System.out.println("First letter: " + word.charAt(word.length() - 1));
+            Character charAtIndexOfWord = word.charAt(1);
+
+        }
+
+        // display results
+        System.out.println("Total Spaces: " + spaces);
+        System.out.println("Total Uppercase Letters: " + upperCase);
+    }
+}
