4751c2964879ca8cf2e6381036e5514c8c89de6a
CompanyProfile <alanood94895@gmail.com>
2026-04-16 10:29:16 +0400

    TASK24:StringLoopValidationSystem

--- src/StringLoopValidationSystem.java
+++ src/StringLoopValidationSystem.java
@@ -29,7 +29,7 @@ public class StringLoopValidationSystem {
             // Get character for checking vowels
             char ch = text.charAt(i);
 
-            // Count vowels
+            // Count Vowels
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
                     || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                 vowels++;
